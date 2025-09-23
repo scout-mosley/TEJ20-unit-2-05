3914a2bbc178fdd656a3cc7e312ce38c0d0a1c9a
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-09-23 12:42:12 -0400

    Shows temperature in kelvins

--- main.ts
+++ main.ts
@@ -2,10 +2,30 @@
  *
  * Created by: scout mosley
  * Created on: Sep 2025
- * This program gets the curent tempature. in K.
+ * This program gets the curent temperature. in K.
 */
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-//comment 
+// our variable for a temperature
+let temperature: number
+let temperatureValueKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // get temperature value
+   temperature = input.temperature()
+   temperatureValueKelvin = (temperature + 273.15)
+   temperatureValueKelvin = Math.round (temperatureValueKelvin)
+
+
+// show output
+basic.clearScreen()
+basic.showString('The temperature is: ' + temperatureValueKelvin.toString()+ 'K' )
+
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
